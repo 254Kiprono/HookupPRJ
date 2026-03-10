fe41327eee6a4069115abe3727b5803ec6fd5759
Kiprono Alex <beralynebera@gmail.com>
2026-03-11 01:09:33 +0300

    changes

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3266206880"
+    serviceWorkerVersion: "1097758672"
   }
 });
